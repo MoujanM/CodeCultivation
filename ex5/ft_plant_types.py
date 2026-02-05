5272cfd6f22d67716a87d2d53684e2dc6725b681
MoujanM <mouzhina@gmail.com>
2026-02-05 20:44:34 +0000

    working...

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -42,7 +42,7 @@ class Plant:
 
     def get_info(self) -> str:
         """
-        Function to access information about current plant status.
+        Function to access information about any plant's status.
         Returns
         -------
             String containing plant type and attributes
@@ -57,7 +57,7 @@ class Plant:
 
 class Flower(Plant):
     """
-    Specialized child class of Plants representing blueprint of Flowers.
+    Specialized child class of Plants, representing blueprint of Flowers.
 
     Attributes
     ----------
@@ -90,48 +90,123 @@ class Flower(Plant):
         self.color = color
 
     def bloom(self) -> str:
+        """Specialised function returns bloom status."""
         return f'{self.name} is blooming beautifully!'
 
     def get_info(self) -> str:
+        """Builds on Plant function to add Flower-specific info."""
         generic_info = super().get_info()
         return f"{generic_info}, {self.color} color"
 
     def special_info(self) -> str:
+        """Plant's generic placeholder is replaced by child info."""
         return self.bloom()
 
 
 class Tree(Plant):
-    def __init__(self, name: str, height: int, age: int, trunk_diameter: int):
+    """
+    Specialized child class of Plants, representing blueprint of Trees.
+
+    Attributes
+    ----------
+        name : str
+            plant name
+        height : int
+            height of plant in cm
+        age : int
+            age of plant in days
+        trunk_diameter : int
+            speciliazed attribute
+    """
+    def __init__(self, name: str, height: int, age: int,
+                 trunk_diameter: int) -> None:
+        """
+        Constructs all necessary attributes for Plant object.
+        Calls super() to set up inherited basic features.
+
+        Parameters
+        ----------
+            name : str
+                plant name
+            height : int
+                height of plant in cm
+            age : int
+                age of plant in days
+            trunk_diameter : int
+                speciliazed attribute
+        """
         super().__init__(name, height, age)
         self.trunk_diameter = trunk_diameter
 
-    def produce_shade(self):
+    def produce_shade(self) -> str:
+        """
+        Specialised function that calculates shade area
+        and returns str statement.
+        """
         shade = 3.14 * ((self.height / 100) ** 2)
         return f'{self.name} provides {shade} square meteres of shade'
 
     def get_info(self) -> str:
+        """Builds on Plant function to add Tree-specific info."""
         generic_info = super().get_info()
         return f"{generic_info}, {self.trunk_diameter}cm diameter"
 
     def special_info(self) -> str:
+        """Plant's generic placeholder is replaced by child info."""
         return self.produce_shade()
 
 
 class Vegetable(Plant):
+    """
+    Specialized child class of Plants, representing blueprint of Trees.
+
+    Attributes
+    ----------
+        name : str
+            plant name
+        height : int
+            height of plant in cm
+        age : int
+            age of plant in days
+        harvest_season : str
+            season vegetable can be harvested
+        nutritional_value : str
+            vegetable's nutritional value
+    """
     def __init__(self, name: str, height: int, age: int,
-                 harvest_season: str, nutritional_value: str):
+                 harvest_season: str, nutritional_value: str) -> None:
+        """
+        Constructs all necessary attributes for Plant object.
+        Calls super() to set up inherited basic features.
+
+        Parameters
+        ----------
+            name : str
+                plant name
+            height : int
+                height of plant in cm
+            age : int
+                age of plant in days
+            harvest_season : str
+                season vegetable can be harvested
+            nutritional_value : str
+                vegetable's nutritional value
+        """
         super().__init__(name, height, age)
         self.harvest_season = harvest_season
         self.nutritional_value = nutritional_value
 
-    def get_nutrition(self):
+    def get_nutrition(self) -> str:
+        """Specialized function to return nutrition."""
         return f'{self.name} is rich in {self.nutritional_value}'
 
     def get_info(self) -> str:
+        """Builds on Plant function to add Tree-specific info."""
         generic_info = super().get_info()
         return f"{generic_info}, {self.harvest_season} harvest"
 
     def special_info(self) -> str:
+        """Plant's generic placeholder is replaced by child info."""
         return self.get_nutrition()
 
 
@@ -141,7 +216,7 @@ if __name__ == "__main__":
     plant3 = Vegetable("Tomato", 80, 90, "summer", "vitamic C")
     my_garden = [plant1, plant2, plant3]
     print("=== Garden Plant Types ===")
-    for i in range(len(my_garden)):
+    for i in range(3):
         plant = my_garden[i]
         print(plant.get_info())
         print(plant.special_info())
--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -10,3 +10,143 @@
 #                                                                           #
 # ************************************************************************* #
 
+class GardenManager:
+    """to handle multiple gardens"""
+    
+    # class-level variable to track all gardens
+    total_gardens = 0
+
+    def __init__(self, garden_owner: str) -> None:
+        """
+        Constructs garden with necessary attributes. 
+        Each construction increments total_gardens.
+        Class holds a list of plants in garden.
+        """
+        self.garden_owner = garden_owner
+        self.plants = []
+        GardenManager.total_gardens += 1
+    
+    @classmethod
+    def create_garden_network(self):
+        pass
+    
+    class GardenStats:
+        """Child class to handle statistics calculations."""
+        @staticmethod
+        def count_types(plants: list) -> dict:
+            
+    
+    
+
+    @staticmethod
+    def height_validation(height) -> bool:
+        """Utility function to validate height > 0"""
+        return height > 0
+
+class Plant:
+    """
+    Parent class.
+    Attributes
+    ----------
+        name : str
+            plant name
+        height : int
+            height of plant in cm
+        age : int
+            age of plant in days
+    """
+    def __init__(self, name: str, height: int, age: int) -> None:
+        """
+        Constructs all necessary attributes for Plant object.
+
+        Parameters
+        ----------
+            name : str
+                plant name
+            height : int
+                height of plant in cm
+            age : int
+                age of plant in days
+        """
+        self.name = name
+        self.height = height
+        self.age = age
+    
+    def grow(self) -> None:
+        """Function to simulate plant growth"""
+        self.height += 1
+
+class FloweringPlant(Plant):
+    """
+    Child class of Plant.
+
+    Attributes
+    ----------
+        name : str
+            plant name
+        height : int
+            height of plant in cm
+        age : int
+            age of plant in days
+        color: str
+            speciliazed attribute
+    """
+    def __init__(self, name: str, height: int, age: int, color: str) -> None:
+        """
+        Constructs all necessary attributes for Plant object.
+        Calls super() to set up inherited basic features.
+
+        Parameters
+        ----------
+            name : str
+                plant name
+            height : int
+                height of plant in cm
+            age : int
+                age of plant in days
+            color: str
+                speciliazed attribute
+        """
+        super().__init__(name, height, age)
+        self.color = color
+
+    def bloom(self) -> str:
+        """Specialised function returns bloom status."""
+        return f'{self.name} is blooming beautifully!'
+
+class PrizeFlower(FloweringPlant):
+    """
+    Grandchild of Plant.
+
+    Attributes
+    ----------
+    name : str
+            plant name
+        height : int
+            height of plant in cm
+        age : int
+            age of plant in days
+        points : int
+            count of prize points
+    """
+    def __init__(self, name: str, height: int, age: int,
+                 points: int) -> None:
+        """
+        Constructs all necessary attributes for Plant object.
+        Calls super() to set up inherited basic features.
+
+        Parameters
+        ----------
+            name : str
+                plant name
+            height : int
+                height of plant in cm
+            age : int
+                age of plant in days
+            color: str
+                speciliazed attribute
+        """
+        super().__init__(name, height, age)
+        self.points = points
+
+
